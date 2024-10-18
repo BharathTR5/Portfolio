6f485640510007db579689d680ef54b2e1457b02
BharathTR <bharathtr70260@gmail.com>
2024-10-18 16:19:28 +0530

    added animation using js

--- index.js
+++ index.js
@@ -4,5 +4,32 @@ let ButtonToggle = document.querySelector('.toggle_btn');
 
     ButtonToggle.addEventListener('click',()=>{
       dropdown_menu.classList.toggle('open');
-      console.log('Toggle button clicked')
     })
+
+window.addEventListener('scroll', ()=> {
+  const screenPosition = window.innerHeight / 1.3;
+  const aboutSection = document.querySelector('#about-container');
+  console.log(aboutSection)
+  const skillsSection = document.querySelector('#skill-container');
+  const resumeSection = document.querySelector('#resume');
+
+  const aboutSectionPosition = aboutSection.getBoundingClientRect().top;
+  const skillsSectionPosition = skillsSection.getBoundingClientRect().top;
+  const resumeSectionPosition = resumeSection.getBoundingClientRect().top;
+
+  if(aboutSectionPosition < screenPosition){
+    aboutSection.style.opacity = '1';
+    aboutSection.style.transform = 'translateY(0)';
+  }
+
+  if(skillsSectionPosition < screenPosition){
+    skillsSection.style.opacity = '1';
+    skillsSection.style.transform = 'translateY(0)';
+  }
+
+  if(resumeSectionPosition < screenPosition){
+    resumeSection.style.opacity = '1';
+    resumeSection.style.transform = 'translateY(0)';
+  }
+
+});
